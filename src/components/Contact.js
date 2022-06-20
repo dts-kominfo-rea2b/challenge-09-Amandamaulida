0919fba0f4d7a6b339c9280a45798746baed2608
amandamaulida <khumaira.amanda@gmail.com>
2022-06-20 21:59:26 +0700

    coba

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,27 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import contacts from '../data/contacts.json'
 
-const Contact = () => {
+
+const Contact = ({data}) => {
+
     return (
-        <></>
+        <>      
+            <div class="container">
+                <div class="card">
+                        <div class="cardTitle">
+                            <div>{data.photo}</div>
+                        </div>
+                        <div class="cardBody">
+                            <div>{data.name}</div>
+                            <div>{data.phone}</div>
+                            <div>{data.email}</div>
+                        </div>
+                </div>
+            </div>
+        
+         </>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,24 @@
 import React from 'react';
 
 const Header = () => {
-    return (
-        <></>
-    )
+    const myStyle = {
+        color: "white",
+        backgroundColor: "DodgerBlue",
+        padding: "10px",
+        fontFamily: "Sans-Serif"
+        
+      };
+      return (
+        <>
+       
+             <div style={myStyle}>
+                <h1 >Call A Friend</h1>
+                <p>Your Freindly contact App</p>
+            </div>
+          
+        </>
+      );
+
 }
 
 export default Header;
